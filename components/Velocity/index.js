ea28d7dbda0c071b8041df566b71040498f7a90b
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2020-05-07 19:49:53 -0700

    fixup to export context components from the library

--- components/Velocity/index.js
+++ components/Velocity/index.js
@@ -4,12 +4,16 @@ import Velocity from "./components/Velocity";
 import VelocityResult from "./components/VelocityResult";
 import VelocityResultList from "./components/VelocityResultList";
 import VelocitySearchField from "./components/VelocitySearchField";
-import VelocityContext, { useVelocityContext } from "./src/context";
+import VelocityContext, {
+	useVelocityContext,
+	VelocityContextProvider,
+} from "./src/context";
 
 export {
 	useVelocityContext,
 	Velocity,
 	VelocityContext,
+	VelocityContextProvider,
 	VelocityResult,
 	VelocityResultList,
 	VelocitySearchField,
