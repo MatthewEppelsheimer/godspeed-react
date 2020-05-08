64bf2c26c3dd176ba4ebab19413a6659a6bda01c
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2020-05-07 17:57:35 -0700

    Implment a basic slotfill

--- components/Velocity/components/VelocityResult.js
+++ components/Velocity/components/VelocityResult.js
@@ -1,10 +1,20 @@
+import { useContext } from 'react';
 import styles from './VelocityResult.module.css'
 import { buildHighlightString } from '../src/selection'
+import VelocityContext from '../src/context';
 
 export default function VelocityResult(props) {
-    const {result, selectedResultIndex, searchPhrase} = props;
+    const {result, selectedResultIndex, searchPhrase } = props;
+
+    const { selection, slotFills } = useContext(VelocityContext);
 
     const inner = '' === searchPhrase ? result.value : buildHighlightString(searchPhrase,result.value,styles.highlight);
 
-    return <li className={result.index === selectedResultIndex ? styles.selected : ""}>{inner}</li>
+    const fill = slotFills && slotFills.resultListItemSlot &&
+        slotFills.resultListItemSlot(
+            result,
+            {delete: selection.delete}
+        ) || null;
+
+    return <li className={result.index === selectedResultIndex ? styles.selected : ""}>{inner}{fill}</li>
 }
