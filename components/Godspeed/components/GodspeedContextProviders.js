4ee6ff93aaf03687f2b9e78d3a0bcff26d6f1de7
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2020-05-12 15:14:57 -0700

    Generalize pattern for immutable contexts

--- components/Godspeed/components/GodspeedContextProviders.js
+++ components/Godspeed/components/GodspeedContextProviders.js
@@ -10,16 +10,14 @@ const GodspeedContextProviders = (props) => {
 	const { deprecatedController, editorController } = controllers;
 	const { getEditors, getState, setState } = editorController;
 
+	// IMMUTABLE CONTEXTS
+	//
 	// state for context values prevents needless consumer renders
 	// see {@link https://reactjs.org/docs/context.html#caveats}
 	// closures passed to useState() mean they'll only run on first render
-	const getInitialEditorContext = () => {
-		return {
-			getState,
-			setState,
-		};
-	};
-	const [editorContext] = useState(() => getInitialEditorContext());
+	const useImmutableContext = (value) => useState(() => value);
+
+	const [editorContext] = useImmutableContext({ getState, setState });
 
 	// Only recreate mutable context values when their dependencies have changed
 	// See {@link https://reactjs.org/docs/hooks-faq.html#how-to-read-an-often-changing-value-from-usecallback}
